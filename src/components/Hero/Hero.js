7c48a22ac60b5d551ef0faa27916b4e4d99c854f
Karthik G <156653445+karthikgrsk@users.noreply.github.com>
2026-06-25 18:17:14 +0530

    Update resume download link in Hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => (
          Software Developer
         , Turning Ideas Into Real Applications
       </SectionText>
-      <Button onClick={ () => {window.location = 'https://drive.google.com/file/d/1mpf5BQESUPfVLVZmX484mVHDqlnfwXPx/view'}}>DownLoad Resume</Button>
+      <Button onClick={ () => {window.location = 'https://drive.google.com/file/d/1eKQGViMuERfAEOSYqNo7susjYrgqykYp/view?usp=sharing'}}>DownLoad Resume</Button>
     </LeftSection>
   </Section>
 );
